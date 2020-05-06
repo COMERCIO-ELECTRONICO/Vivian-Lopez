62f25d883e904b4f7614455badf8c2a5c907ba50
Vivian <vtipan19@hotmail.com>
2020-05-06 12:48:31 -0500

    Clase 1

---
+++ 01-javascript.js
@@ -0,0 +1,17 @@
+// TIPO DE DATOS (NUMBER), STRING,BOOL
+// VAR - CONST ->
+
+var json = {
+nombre: 'Vivian',
+edad: 20,
+soltero: true,
+fecha: new Date(1999 - 09 - 19),
+}
+console.log(json.nombre)
+console.log(json['edad'])
+json.nombre = 'nuevo'
+json.direccion = 'sol'
+/*console.error()
+console.info()
+console.warn()*/
+ 
