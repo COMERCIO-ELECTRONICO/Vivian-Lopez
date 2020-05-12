c7f9d881df0ce19c9b35263b5825a7ee8fe9fb6a
Vivian <vtipan19@hotmail.com>
2020-05-12 12:15:55 -0500

    tcs_ejercicio2

--- typescript/tipo-dato.ts
+++ typescript/tipo-dato.ts
@@ -39,3 +39,26 @@ sumar();
 console.log(sumar(5,5));
 console.log(sumar(5));
 console.log(sumar(5));
+
+class Persona {
+    constructor(
+        protected nombre?: string,
+        public apellido?: string
+    ) {
+
+    }
+}
+const nnuevaPersona = new Persona()
+
+class hijo extends Persona {
+    constructor(
+        protected nombre: string,
+        public apellido: string,
+    ) {
+        super(nombre, apellido);
+        this.apellido = apellido;
+    }
+}
+
+const hijoPersona = new hijo('vivian', 'lopez');
+console.log(hijoPersona);
