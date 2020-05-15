a272f6f96d1774535cffac19f329f19b60d700cc
Vivian <vtipan19@hotmail.com>
2020-05-15 15:55:27 -0500

    boopstrap

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
 import { PadreComponent } from './padre/padre.component';
 import { HijoComponent } from './hijo/hijo.component';
+import { ProyectoAngularComponent } from './proyecto-angular/proyecto-angular.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiPrimerComponenteComponent,
     PadreComponent,
-    HijoComponent
+    HijoComponent,
+    ProyectoAngularComponent
   ],
   imports: [
     BrowserModule,
